5317753b11cd854dccc85f802213261637bbd4f5
Jamie Ferguson <jppferguson@gmail.com>
2013-08-12 12:34:56 +0100

    Initial Commit

---
+++ css/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "../"
+css_dir = ""
+sass_dir = "sass"
+
+output_style = :nested
+environment = :development
+
+line_comments = false
+color_output = false
+preferred_syntax = :scss
